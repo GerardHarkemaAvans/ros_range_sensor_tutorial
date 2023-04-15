a8956b6d126fe99d5ae2d9d74354746a535ef137
Gerard Harkema <ga.harkema@avans.nl>
2023-04-15 12:38:12 +0200

    20230415-3

--- range_sensor/script/assignment1.py
+++ range_sensor/script/assignment1.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Assignment 1 for Week1: In this assignment you will subscribe to the topic that
+# Assignment 1: In this assignment you will subscribe to the topic that
 # publishes sensor information. Then, you will transform the sensor reading from
 # the reference frame of the sensor to compute the height of a box based on the
 # illustration shown in the assignment document. Then, you will publish the box height
@@ -8,28 +8,25 @@
 
 # All necessary python imports go here.
 import rospy
-from range_sensor_msgs.msg import BoxHeightInformation
+from range_sensor_msgs.msg import <write-your-code-here-Part1>
 from sensor_msgs.msg import Range
-
-_SENSOR_CONVEYOR_BELT_DISTANCE = 1.0 # Metres -> assume that the sensor is placed 1.0 above the conveyor belt
-_SENSOR_USABLE_RANGE = 0.9 # Metres -> Usable sensor range, accordingly datasheet
 
 def sensor_info_callback(data, bhi_pub):
 
-    box_distance = data.range
+    height_box = <write-your-code-here-Part1>
 
     # Compute the height of the box.
     # Boxes that are detected to be shorter than 10cm are due to sensor noise.
     # Do not publish information about them.
-    if box_distance > _SENSOR_USABLE_RANGE:
+    if <write-your-code-here-Part1>:
         pass
     else:
         # Declare a message object for publishing the box height information.
         box_height_info = BoxHeightInformation()
         # Update height of box.
-        box_height_info.box_height = _SENSOR_CONVEYOR_BELT_DISTANCE - box_distance
+        <write-your-code-here-Part3>
         # Publish box height using the publisher argument passed to the callback function.
-        bhi_pub.publish(box_height_info)
+        <write-your-code-here-Part3>
 
 if __name__ == '__main__':
     # Initialize the ROS node here.
@@ -37,11 +34,11 @@ if __name__ == '__main__':
 
     # Wait for the topic that publishes sensor information to become available - Part1
     rospy.loginfo('Waiting for topic %s to be published...', 'sensor_info')
-    rospy.wait_for_message('sensor_info', Range)
-    rospy.loginfo('%s topic is now available!', 'sensor_info')
+    rospy.wait_for_message('<use the correct topic name here>', <use the correct message type here>)
+    rospy.loginfo('%s topic is now available!', <use the correct topic name here>)
 
     # Create the publisher for Part3 here
-    bhi_publisher = rospy.Publisher('box_height_info', BoxHeightInformation, queue_size=10)
+    bhi_publisher = rospy.Publisher('<use correct topic name here>', <use correct message type here>, queue_size=10)
     # Note here that an ADDITIONAL ARGUMENT (bhi_publisher) is passed to the subscriber. This is a way to pass
     # ONE additional argument to the subscriber callback. If you want to pass multiple arguments,
     # you can use a python dictionary. And if you don't want to use multiple arguments to the
@@ -49,7 +46,7 @@ if __name__ == '__main__':
     # the action server code illustration.
 
     # Create the publisher for Part1 here
-    rospy.Subscriber('sensor_info', Range, sensor_info_callback, bhi_publisher)
+    rospy.Subscriber('<use correct topic name here>', <use correct message type here>, <use the correct callback name here>, bhi_publisher)
 
     # Prevent this code from exiting until Ctrl+C is pressed.
     rospy.spin()
--- range_sensor/script/assignment2.py
+++ range_sensor/script/assignment2.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
 
-# Assignment 2 for Week1: In this assignment you will subscribe to the topic that
+# Assignment 2: In this assignment you will subscribe to the topic that
 # publishes information on the box height in metres and use the metres_to_feet
 # service to convert this height in metres to height in feet.
-
 
 import rospy
 from range_sensor_msgs.msg import BoxHeightInformation
@@ -12,19 +11,13 @@ from range_sensor_msgs.srv import ConvertMetresToFeet, ConvertMetresToFeetReques
 def box_height_info_callback(data):
     try:
         # Create a proxy for the service to convert metres to feet - Part2.
-        metres_to_feet = rospy.ServiceProxy('metres_to_feet', ConvertMetresToFeet)
-
-        # Extract box_height_meters from data
-	box_height_meters = data.box_height
+        metres_to_feet = rospy.ServiceProxy(<update the correct details here>)
 
         # Call the service here.
-        service_response = metres_to_feet(box_height_meters)
+        service_response = <write-your-code-here-Part2>
 
         # Extract box_height_feet from service_response
-        box_height_feet = service_response.distance_feet
-
-        # Write a log message here to print the height of this box in feet.
-        rospy.loginfo("The hight of the box is %4.2f metres or %4.2f feet" %(box_height_meters, box_height_feet)) 
+        <write-your-code-here-Part2>
 
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
@@ -35,11 +28,11 @@ if __name__ == '__main__':
 
     # First wait for the service to become available - Part2.
     rospy.loginfo("Waiting for service...")
-    rospy.wait_for_service('metres_to_feet')
-    rospy.loginfo("Service %s is now available", 'metres_to_feet')
+    rospy.wait_for_service('<use the correct service name here>')
+    rospy.loginfo("Service %s is now available", '<use the correct service name here>')
 
     # Create a subscriber to the box height topic - Part1.
-    rospy.Subscriber('box_height_info', BoxHeightInformation, box_height_info_callback)
+    rospy.Subscriber(<use the correct topic name here>, <use the correct message type here>, <use the correct callback name here>)
 
     # Prevent this ROS node from terminating until Ctrl+C is pressed.
     rospy.spin()
